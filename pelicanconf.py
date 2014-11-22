f4f96bf126f2df9a71d4d1466484416d4dc9c683
Ryan M <rjames86@gmail.com>
2014-11-22 09:38:05 -0800

    initial add

---
+++ pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ryan M'
+SITENAME = u'Tuckshop Coffee'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'themes/pure'
+
+STATIC_PATHS = ['images']
+
+
+######################
+# Theme specific stuff
+######################
+
+COVER_IMG_URL = 'images/banner.jpg'
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
